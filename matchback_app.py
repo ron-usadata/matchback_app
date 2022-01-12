8c2f1be7d8b48e2e074c16e8cefb6a9076e93d18
unknown <rwalters@usadata.com>
2022-01-12 14:37:35 -0500

    Enabled download of grouping and multi selection

--- matchback_app.py
+++ matchback_app.py
@@ -1,18 +1,26 @@
 import streamlit as st
 import pandas as pd
+import io
 
 
-@st.cache
 def convert_df(df):
-    return df.to_csv().encode("utf-8")
+    if type(df) == type([]):
+        buffer = io.BytesIO()
+        with pd.ExcelWriter(buffer) as writer:
+            df[0].to_excel(writer, sheet_name="Matches", index=False)
+            df[1].to_excel(writer, sheet_name="Grouping Counts")
+        return buffer
+    else:
+        return df.to_csv(index=False).encode("utf-8")
 
 
 def create_matches():
     if prospect_file is None or customer_file is None:
         st.error("Please select prospect and/or customer file")
+        return False
     # elif save_path == "" or file_name == "":
     #    st.error("Please enter save path and/or file name")
-    elif "person_hash" in [
+    elif " " in [
         prosp_addr,
         prosp_ln,
         prosp_zip,
@@ -21,6 +29,7 @@ def create_matches():
         cust_zip,
     ]:
         st.error("Please select prospect and customer columns")
+        return False
     else:
         prospects["jr_string"] = prospects.apply(
             lambda row: str(row[prosp_addr]).lower().strip()[:10]
@@ -56,8 +65,8 @@ def create_matches():
 st.title("USAData Matchback")
 
 # Set empty array for column list for loading purposes
-prospect_columns = []
-customer_columns = []
+prospect_columns = [" "]
+customer_columns = [" "]
 
 
 # file selection for prospect file with formatting for column selection
@@ -77,7 +86,7 @@ st.text(
 # When a file is selected, load into dataframe and allow column selection
 if prospect_file is not None:
     prospects = pd.read_csv(prospect_file)
-    prospect_columns = prospects.columns
+    prospect_columns += prospects.columns.tolist()
     with col1:
         prosp_addr = st.selectbox("Select Prospect Address Column", prospect_columns)
     with col2:
@@ -88,7 +97,7 @@ if prospect_file is not None:
 
 if customer_file is not None:
     customers = pd.read_csv(customer_file)
-    customer_columns = customers.columns
+    customer_columns += customers.columns.tolist()
     with col4:
         cust_addr = st.selectbox("Select Customer Address Column", customer_columns)
     with col5:
@@ -99,27 +108,63 @@ if customer_file is not None:
 col7, col8, col9 = st.columns(3)
 with col7:
     match_button = st.button("Create Matches")
+
 if match_button:
     matches = create_matches()
-    st.session_state["df"] = matches
+    if type(matches) == type(pd.DataFrame()):
+        st.session_state["df"] = matches
 
 if "df" in st.session_state:
     # Path and file name for resulting match file
-    csv = convert_df(st.session_state.df)
-    st.text(f"Total Matches: {len(st.session_state.df)}")
+    st.header(f"Total Matches: {len(st.session_state.df)}")
     file_name = st.text_input("File Name (Without Extension)")
-    with col9:
-        st.download_button(
-            "Download Match File",
-            data=csv,
-            file_name=(f"{file_name}.csv" if file_name != "" else "match_file.csv"),
-            mime="text/csv",
+
+    df = pd.DataFrame()
+    cols = [" "] + st.session_state.df.columns.tolist()
+    col10, col11 = st.columns(2)
+    with col10:
+        grp_col1 = st.selectbox("Select grouping column 1", cols)
+    with col11:
+        grp_col2 = st.selectbox("Select grouping column 2 (optional)", options=cols)
+
+    if " " not in [grp_col1, grp_col2]:
+        df = (
+            st.session_state.df.groupby([grp_col1, grp_col2], dropna=False)
+            .count()
+            .rename(columns={cols[1]: "Count"})
+            .iloc[:, 0]
         )
-    cols = st.session_state.df.columns
-    grp_col = st.selectbox("Select grouping column", cols)
-    st.dataframe(
-        st.session_state.df.groupby(grp_col, dropna=False)
-        .count()
-        .rename(columns={cols[0]: "Count"})
-        .iloc[:, 0]
-    )
+
+    elif " " != grp_col1:
+        df = (
+            st.session_state.df.groupby(grp_col1, dropna=False)
+            .count()
+            .rename(columns={cols[1]: "Count"})
+            .iloc[:, 0]
+        )
+    if not df.empty:
+        st.dataframe(df)
+    with col9:
+        download_btn = st.empty()
+        breakdown = st.checkbox("Include grouping?")
+        if breakdown:
+            csv = convert_df([st.session_state.df, df])
+            with download_btn:
+                st.download_button(
+                    "Download Match File",
+                    data=csv,
+                    file_name=(
+                        f"{file_name}.xlsx" if file_name != "" else "match_file.xlsx"
+                    ),
+                )
+        else:
+            csv = convert_df(st.session_state.df)
+            with download_btn:
+                st.download_button(
+                    "Download Match File",
+                    data=csv,
+                    file_name=(
+                        f"{file_name}.csv" if file_name != "" else "match_file.csv"
+                    ),
+                    mime="text/csv",
+                )
