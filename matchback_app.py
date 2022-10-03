735d8c8278984af06990abde22e1b382b052850b
unknown <rwalters@usadata.com>
2022-10-03 14:57:13 -0400

    Added verification of person_hash, removed liveramp check

--- matchback_app.py
+++ matchback_app.py
@@ -14,8 +14,7 @@ def convert_df(df):
         return df.to_csv(index=False).encode("utf-8")
 
 
-def create_matches(liveramp):
-    print(cust_dedupe)
+def create_matches():
     if prospect_file is None or customer_file is None:
         st.error("Please select prospect and/or customer file")
         return False
@@ -31,21 +30,25 @@ def create_matches(liveramp):
     ]:
         st.error("Please select prospect and customer columns")
         return False
-    elif len(cust_dedupe) == 0:
-        st.error("Please select column(s) for deduplicating")
-        return False
     else:
-        customers.drop_duplicates(subset=cust_dedupe, inplace=True)
+        prospects = pd.read_csv(prospect_file, dtype={prosp_zip:str})
+        customers = pd.read_csv(customer_file, dtype={cust_zip:str})
+        if 'person_hash' not in prospects.columns:
+            prospects.reset_index(inplace=True)
+            prospects.rename(columns={'index':'person_hash'}, inplace=True)
+        if 'person_hash' not in customers.columns:
+            customers.reset_index(inplace=True)
+            customers.rename(columns={'index':'person_hash'}, inplace=True)
         prospects["jr_string"] = prospects.apply(
             lambda row: str(row[prosp_addr]).lower().strip()[:10]
             + str(row[prosp_ln]).lower().strip()[:2]
-            + str(int(float(row[prosp_zip]))),
+            + row[prosp_zip],
             axis=1,
         )
         customers["jr_string"] = customers.apply(
             lambda row: str(row[cust_addr]).lower().strip()[:10]
             + str(row[cust_ln]).lower().strip()[:2]
-            + str(int(float(row[cust_zip]))),
+            + row[cust_zip],
             axis=1,
         )
         jr_matches = prospects.merge(
@@ -55,7 +58,7 @@ def create_matches(liveramp):
         abil_matches = prospects.merge(
             customers,
             how="inner",
-            on= "household_link" if liveramp else "household.abilitec.householdLink",
+            on= "household_link",
             suffixes=["_Prospect", "_Customer"],
         )
         abil_matches["match_type"] = "Abilitec"
@@ -84,7 +87,7 @@ customer_columns = [" "]
 
 
 # file selection for prospect file with formatting for column selection
-prospect_file = st.file_uploader("Select Prospect File")
+prospect_file = st.file_uploader("Select Prospect/Audience File")
 col1, col2, col3 = st.columns(3)
 st.text(
     "-----------------------------------------------------------------------------------"
@@ -100,8 +103,7 @@ st.text(
 
 # When a file is selected, load into dataframe and allow column selection
 if prospect_file is not None:
-    prospects = pd.read_csv(prospect_file)
-    prospect_columns += prospects.columns.tolist()
+    prospect_columns += pd.read_csv(prospect_file, nrows=0).columns.tolist()
     with col1:
         prosp_addr = st.selectbox("Select Prospect Address Column", prospect_columns)
     with col2:
@@ -111,27 +113,20 @@ if prospect_file is not None:
 
 
 if customer_file is not None:
-    customers = pd.read_csv(customer_file)
-    customer_columns += customers.columns.tolist()
+    customer_columns += pd.read_csv(customer_file, nrows=0).columns.tolist()
     with col4:
         cust_addr = st.selectbox("Select Customer Address Column", customer_columns)
     with col5:
         cust_ln = st.selectbox("Select Customer Lastname Column", customer_columns)
     with col6:
         cust_zip = st.selectbox("Select Customer Zip Column", customer_columns)
-    with col_ded:
-        cust_dedupe = st.multiselect(
-            "Select Column(s) to Dedupe From", customer_columns
-        )
 
 col7, col8, col9 = st.columns(3)
 with col7:
     match_button = st.button("Create Matches")
-with col8:
-    liveramp = st.checkbox("Liveramp")
 
 if match_button:
-    matches = create_matches(liveramp)
+    matches = create_matches()
     if type(matches) == type(pd.DataFrame()):
         st.session_state["df"] = matches
 
